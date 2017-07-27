7c8e5ae9dd29c4a81829e4f5dddaa2a32a7efc8d
Arne <bratt@nvg.ntnu.no>
2017-07-27 09:56:22 +0200

    Minor adjustment

--- models/json.js
+++ models/json.js
@@ -6,6 +6,6 @@ var DataSchema = new Schema({
     domainId: {type: ObjectId, ref: "Domain", index: true},
     hash: {type: String, index: true},
     json: {type: String}
-}, { collection: 'mirrorjson_api_data2' });
+}, { collection: 'mirrorjson_api_data' });
 
 mongoose.model('Data', DataSchema);
