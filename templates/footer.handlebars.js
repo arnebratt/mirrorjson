a087dded081ae7ddc8057a6dabccd7e7770dff42
Arne <bratt@nvg.ntnu.no>
2017-08-16 09:29:28 +0200

    Minor fix version

--- templates/footer.handlebars.js
+++ templates/footer.handlebars.js
@@ -1,6 +1,6 @@
 let tpl = `
     <hr />
-    <address>MirrorJson version 0.90. Idea, design and implementation by Arne Bakkebø, Making Waves in 2017.</address>
+    <address>MirrorJson version 0.91. Idea, design and implementation by Arne Bakkebø, Making Waves in 2017.</address>
     </body>
 </html>
 `;
